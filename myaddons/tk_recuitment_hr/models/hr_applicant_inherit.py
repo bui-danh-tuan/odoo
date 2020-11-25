1846cdd2257aad700e3fbe2dcb4a3338a3f90df2
bui-danh-tuan <buidanhtuan22121999@gmail.com>
2020-11-25 12:27:20 +0700

    update

--- myaddons/tk_recuitment_hr/models/hr_applicant_inherit.py
+++ myaddons/tk_recuitment_hr/models/hr_applicant_inherit.py
@@ -6,6 +6,7 @@ class Applicant(models.Model):
 
     employee_id = fields.Many2one(string='Employee introduce',comodel_name='hr.employee',ondelete='restrict',)
     name_source = fields.Char()
+    set_criterias_id = fields.Many2one('tk_recruitment_hr.set_criterias','Set critetias',ondelete='restrict',related='job_id.set_criterias_id')
     
     @api.onchange('source_id')
     def _onchange_field(self):
