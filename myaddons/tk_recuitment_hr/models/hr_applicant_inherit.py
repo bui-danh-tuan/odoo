5e196e93600b1ce9da76399f4c446243e9fa1355
bui-danh-tuan <buidanhtuan22121999@gmail.com>
2020-11-25 11:43:45 +0700

    applicant add emplyee

--- myaddons/tk_recuitment_hr/models/hr_applicant_inherit.py
+++ myaddons/tk_recuitment_hr/models/hr_applicant_inherit.py
@@ -3,4 +3,14 @@ from odoo.exceptions import UserError, ValidationError
 
 class Applicant(models.Model):
     _inherit = "hr.applicant"
+
+    employee_id = fields.Many2one(string='Employee introduce',comodel_name='hr.employee',ondelete='restrict',)
+    name_source = fields.Char()
+    
+    @api.onchange('source_id')
+    def _onchange_field(self):
+        self.name_source = self.source_id.name
+        print(self.name_source)
+    
+    
     
